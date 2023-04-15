3065517e6cc717c6e6dbfcdae04dac9df63201fd
karam-zomlut <zomlutk@gmail.com>
2023-04-15 19:29:48 +0200

    Push project

---
+++ bin/create-express-app.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import { exec } from 'child_process';
+import chalk from 'chalk';
+import generateExpressStructure from '../index.js';
+
+const args = process.argv.slice(2);
+const directoryName = args[0] || 'my-express-app';
+
+generateExpressStructure(directoryName);
+
+console.log(`\nInstalling dependencies...`);
+const installDependencies = exec(`cd ${directoryName} && npm i express dotenv cross-env compression && npm i nodemon -D && cd ..`);
+
+installDependencies.stdout.on('data', (data) => {
+  console.log(data);
+});
+
+installDependencies.stderr.on('data', (data) => {
+  console.log(data);
+});
+
+installDependencies.on('exit', (code) => {
+
+  if (code !== 0) {
+    console.log(chalk.bgRed(`\nDependencies installation failed!`));
+    console.log(chalk.bgRed(`\nPlease run the following commands:`));
+    console.log(`\ncd ${directoryName}`);
+    console.log('npm i express dotenv cross-env compression');
+    console.log('npm i nodemon -D');
+    return;
+  };
+
+  console.log(`\nDependencies installed successfully!`);
+  console.log('\n' + chalk.green('Get Started:'));
+  console.log(chalk.cyan(`\ncd ${directoryName}`));
+  console.log(chalk.cyan('npm run dev'));
+  console.log(chalk.bgBlue('\nHappy hacking!'));
+});
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+import { mkdirSync, writeFileSync } from 'fs';
+import { appFile, controllerIndexFile, homeControllerFile, indexFile, routerIndexFile } from './fielsContent.js';
+
+function generateExpressStructure(dirName) {
+  // Create the main server directory
+  mkdirSync(dirName);
+
+  mkdirSync(`${dirName}/server`);
+  mkdirSync(`${dirName}/public`);
+
+  // Create the subdirectories of the server directory
+  mkdirSync(`${dirName}/server/controller`);
+  mkdirSync(`${dirName}/server/database`);
+  mkdirSync(`${dirName}/server/database/config`);
+  mkdirSync(`${dirName}/server/database/query`);
+  mkdirSync(`${dirName}/server/router`);
+  mkdirSync(`${dirName}/server/utils`);
+  mkdirSync(`${dirName}/server/utils/validation`);
+  mkdirSync(`${dirName}/server/utils/jwt`);
+  mkdirSync(`${dirName}/server/middlewares`);
+
+  // Create the other necessary files for the express app
+  writeFileSync(`${dirName}/server/index.js`, indexFile);
+  writeFileSync(`${dirName}/server/app.js`, appFile);
+  writeFileSync(`${dirName}/server/controller/homeController.js`, homeControllerFile);
+  writeFileSync(`${dirName}/server/controller/index.js`, controllerIndexFile);
+  writeFileSync(`${dirName}/server/database/index.js`, '');
+  writeFileSync(`${dirName}/server/database/config/index.js`, '');
+  writeFileSync(`${dirName}/server/database/query/index.js`, '');
+  writeFileSync(`${dirName}/server/router/index.js`, routerIndexFile);
+  writeFileSync(`${dirName}/server/utils/index.js`, '');
+  writeFileSync(`${dirName}/server/utils/validation/index.js`, '');
+  writeFileSync(`${dirName}/server/utils/jwt/index.js`, '');
+  writeFileSync(`${dirName}/server/middlewares/index.js`, '');
+  writeFileSync(`${dirName}/.gitignore`, 'node_modules/\n.env');
+  writeFileSync(`${dirName}/.env`, 'PORT=8080');
+  writeFileSync(`${dirName}/example.env`, '');
+  writeFileSync(`${dirName}/package.json`, JSON.stringify({
+    name: dirName,
+    version: '1.0.0',
+    description: '',
+    main: 'index.js',
+    scripts: {
+      dev: 'cross-env NODE_ENV=development nodemon server/index.js',
+      start: 'cross-env NODE_ENV=production node server/index.js'
+    }
+  }, null, 2));
+}
+
+export default generateExpressStructure;
